6c95c97f8d9d5fcba66f5696e1f3cc4a8224f792
dot <1310769510@qq.com>
2025-09-15 03:00:54 +0800

    fix: update base, fix problem when env not start at root

--- lib/base.ps1
+++ lib/base.ps1
@@ -2,21 +2,15 @@
 $env:PSModulePath     = $PWD.ToString()+"lib"
 #$HOME                = $PWD.ToString()+"\home"
 remove-variable -force HOME
-$HOME                 = $PWD.ToString()+"home"
+$HOME                 = $PWD.ToString()+($PWD.Path.EndsWith('\') ? "" : "\")+"home"
 
-$env:XDG_HOME_DIR     = $PWD.ToString()+"home"
-$env:XDG_DATA_HOME    = $PWD.ToString()+"home\.local\share"
-$env:XDG_CONFIG_HOME  = $PWD.ToString()+"home\.config"
-$env:XDG_STATE_HOME   = $PWD.ToString()+"home\.local\state"
-$env:HOME             = $PWD.ToString()+"home"
+$env:XDG_HOME_DIR     = $HOME
+$env:XDG_DATA_HOME    = $env:XDG_HOME_DIR+"\.local\share"
+$env:XDG_CONFIG_HOME  = $env:XDG_HOME_DIR+"\.config"
+$env:XDG_STATE_HOME   = $env:XDG_HOME_DIR+"\.local\state"
+$env:HOME             = $HOME
 $env:EDITOR           = 'nvim'
-$PROFILE              = $PWD.ToSTring()+"home\profile.ps1"
-
-$env:RUSTUP_DIST_SERVER = "https://mirrors.ustc.edu.cn/rust-static"
-$env:RUSTUP_UPDATE_ROOT = "https://mirrors.ustc.edu.cn/rust-static/rustup"
-$env:RUSTUP_HOME        = $PWD.ToString() + "PKGs\Store\PLE\RustUp"
-$env:CARGO_HOME        = $PWD.ToString() + "PKGs\Store\PLE\Cargo"
-
+$PROFILE              = $HOME+"\profile.ps1"
 
 Import-Module -Name libLoad -Verbose
 
